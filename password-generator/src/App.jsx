fa12fae56100b63f43a48f46a9f602e7ad64340c
Pratibha1403 <101884035+Pratibha1403@users.noreply.github.com>
2024-10-28 21:28:49 +0530

    new chanages

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [password, setPassword] = useState("");
+  const [isNumberAllowed, setIsNumberAllowed] = useState(false)
+  const [isCharAllowed, setIsCharAllowed] = useState(false)
 
   return (
-    <>
-    <h1 className='bg-red-400'>Set-Up Ready</h1>
-    </>
-  )
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center mb-4">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden">
+        <input type="text" value={password} className="w-full py-1 px-3" />
+        <button className="text-white bg-blue-700 px-3">Copy</button>
+      </div>
+      <div className="flex gap-x-2 text-sm">
+        <div>
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            name=""
+            id=""
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+          />
+          {length}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
